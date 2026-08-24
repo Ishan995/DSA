e54f4f059e873b9a512fed92bb8934d7321c1886
Ishan <ishanst995@gmail.com>
2026-08-24 19:39:36 +0530

    Time: 315 ms (48.62%), Space: 19.3 MB (32.48%) - LeetHub

---
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.py
@@ -0,0 +1,23 @@
+class Solution:
+    def fun(self, m: int, n: int, guess: int) -> int:
+        count = 0
+        for i in range(1, m + 1):
+            count += min(guess // i, n)
+        return count
+
+    def findKthNumber(self, m: int, n: int, k: int) -> int:
+        low = 1
+        high = m * n
+        res = -1
+
+        while low <= high:
+            guess = (low + high) // 2
+            ans = self.fun(m, n, guess)
+
+            if ans < k:
+                low = guess + 1
+            else:
+                res = guess
+                high = guess - 1
+
+        return res
