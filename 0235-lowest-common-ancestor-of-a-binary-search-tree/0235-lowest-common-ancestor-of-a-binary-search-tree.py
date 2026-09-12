df12bf0e7e419e0290971e9072628ef398f8ff12
Ishan <ishanst995@gmail.com>
2026-09-13 03:13:07 +0530

    Time: 54 ms (95.96%), Space: 22.1 MB (76.19%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,26 +7,38 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        ans=None
-        def fun(root,p,q):
-            #assume p.val<q.val
-            nonlocal ans 
-            if not root:
-                return ans 
-            if root.val==p.val or root.val==q.val:
-                ans=root
-                return ans
-            elif root.val>q.val>p.val: 
-                fun(root.left,p,q)
-            elif root.val<p.val<q.val: 
-                fun(root.right,p,q)
-            elif (root.val>p.val and root.val<q.val):
-                ans=root
-                return ans
-            
-        if p.val<q.val:
-            fun(root,p,q)
-        else:
-            fun(root,q,p)
-        return ans
+        ans = None
 
+        def fun(root, p, q):
+            # Assumes p.val < q.val
+            nonlocal ans 
+            
+            if not root:
+                return 
+            
+            if root.val == p.val or root.val == q.val:
+                ans = root
+                return 
+            
+            # If current root is larger than the bigger target (q), 
+            # both p and q must be in the left subtree.
+            elif root.val > q.val: 
+                fun(root.left, p, q)
+            
+            # If current root is smaller than the smaller target (p), 
+            # both p and q must be in the right subtree.
+            elif root.val < p.val: 
+                fun(root.right, p, q)
+            
+            # Current root is strictly between p and q (p.val < root.val < q.val).
+            # The paths split here, so this node is the Lowest Common Ancestor.
+            else:
+                ans = root
+                return 
+
+        if p.val < q.val:
+            fun(root, p, q)
+        else:
+            fun(root, q, p)
+            
+        return ans
