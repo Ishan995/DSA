cdd34bcbefca52e8f0772cac408593789f43a7f5
Ishan <ishanst995@gmail.com>
2026-09-19 17:05:34 +0530

    Time: 3 ms (77.87%), Space: 21 MB (77.45%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        idx = 0 #index of preorder array
+        idx = 0 #index of preorder array #start from first index because preorder is root,left,right
         inorder_map = {}
 
         # 1. Store inorder value -> index for O(1) lookup
