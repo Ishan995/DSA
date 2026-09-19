1621a304cfdb8f9f077eb5381525fd65a668b379
Ishan <ishanst995@gmail.com>
2026-09-19 16:41:15 +0530

    Time: 1 ms (84.53%), Space: 20.8 MB (93.37%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,25 +5,35 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def buildTree(self, preorder: list[int], inorder: list[int]) -> TreeNode | None:
-        idx=0
-        #hasmap for lookup in inorder array
-        in_map={}
-        for index,val in enumerate(inorder):
-            in_map[val]=index
-        def fun(preorder,low,high):
-            nonlocal idx
-            if low>high:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        idx = 0
+        inorder_map = {}
+
+        # 1. Store inorder value -> index for O(1) lookup
+        for index, val in enumerate(inorder):
+            inorder_map[val] = index
+
+        def fun(preorder, low, high):
+            nonlocal idx  
+            
+            # Base case: valid inorder range exhausted
+            if low > high:
                 return None
-            node=TreeNode(preorder[idx])
-            idx+=1
-            id=in_map[node.val]
-            node.left=fun(preorder,low,id-1)
-            node.right=fun(preorder,id+1,high)
+
+            # 2. First element in preorder range is always current root
+            node = TreeNode(preorder[idx])
+            idx += 1
+
+            # 3. Find root location in inorder array to divide subtrees
+            id = inorder_map[node.val]
+
+            # 4. Recursively build left (low to id-1) then right (id+1 to high)
+            node.left = fun(preorder, low, id - 1)
+            node.right = fun(preorder, id + 1, high)
+
             return node
-        return fun(preorder,0,len(inorder)-1)
 
-
+        return fun(preorder, 0, len(inorder) - 1)
 
 
 
